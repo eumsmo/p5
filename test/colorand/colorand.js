9cd1fdf2a3d0a2113fd704f32ee9109c918d6d54
Juan Vitor <juanriven10@gmail.com>
2018-03-25 18:50:13 -0300

    🦆 ColoRand Explain

--- test/colorand/colorand.js
+++ test/colorand/colorand.js
@@ -1,31 +1,41 @@
-
-const positive = num=> num>=0? num : -num;
-const negative = num=> num<0? num: -num;
 const randomColor = () => [random(255),random(255),random(255)];
-let points= [],is_background = (location.search=="?background"); 
+let points= [],
+    is_background = (location.search=="?background");
 
-/* Define Class */
+/* Define Classe */
 class Point{
   constructor(x=100,y=200){
+    // Define posição inicial
     this.x = x;
     this.y = y;
+
+    // Define cor da partícula
     this.color = randomColor();
+
+    // Define tamanho da "cabeça"
     this.size = 5;
+
+    // Define history e a quantidade maxima de posições salvas
     this.history = [];
     this.history_max = 50;
   }
 
   show(){
+    // Coloca cor
     fill(...this.color);
     stroke(...this.color);
+
+    // Cria cabeça
     ellipse(this.x,this.y,this.size,this.size);
 
-    noFill();
+    // Cauda movimenta
     for (let i = 0; i < this.history.length; i++) {
       this.history[i].x += random(-2,2);
       this.history[i].y += random(-2,2);
     }
 
+    // Cria cauda
+    noFill();
     beginShape();
     this.history.forEach(pos=>{
       vertex(pos.x,pos.y);
@@ -34,25 +44,24 @@ class Point{
   }
 
   update(){
+    // Movimenta cabeça
     this.x += random(-15,15);
     this.y += random(-15,15);
 
+    // Compara se passou do limite
     if(this.x>width) this.x=width;
     else if(this.x<0) this.x=0;
 
     if(this.y>height) this.y=height;
     else if(this.y<0) this.y = 0;
 
+    // Armazena posição atual para cauda
     let v = createVector(this.x,this.y);
     this.history.push(v);
 
+    // Elimina excesso de posições salvas
     if(this.history.length>this.history_max)
       this.history.shift();
-  }
-
-  dir(x,y){
-    this.xspeed = this.speedRate*x;
-    this.yspeed = this.speedRate*y;
   }
 
   static create(x,y){
@@ -60,17 +69,21 @@ class Point{
   }
 }
 
+/* Ajusta canvas */
 function setup() {
   createCanvas(windowWidth,windowHeight);
   background(0);
 }
 
-function windowResized() {
+function windowResized() {]
   resizeCanvas(windowWidth, windowHeight);
 }
 
 function draw(){
+  // Se houver "?background" nos parametros, atualizar fundo
   if(is_background) background(0);
+
+  // Atualizar e mostrar todos pontos
   points.forEach(point=>{
     point.update();
     point.show();
